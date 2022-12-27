af17a956e82419b7cd60ffbe7f09920a6205348e
annajedrzejko <anna.jedrzejko@elpassion.pl>
2022-12-27 12:46:43 +0100

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&amp;display=swap"
+          rel="stylesheet"></link>
+        <body className="antialiased bg-body text-body font-body">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
